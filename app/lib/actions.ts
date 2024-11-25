418c40bf22cbe51542109147fa220442df3ffc0e
jeevan <jeevanbeniwal@gmail.com>
2024-11-25 09:24:57 +0530

    Removed the explicit error ingestion from delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -62,7 +62,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete invoice');
+    // throw new Error('Failed to Delete invoice'); // added for testing 
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
